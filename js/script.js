5ed3de965c82c7d4b821430eed2538c643cf9ea9
FaiTech25 <faisal25051997@gmail.com>
2024-11-20 21:28:26 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
 // Ini Javascript
+
+
+function validateForm() {
+    const usernameInput = document.getElementById('username-input').value;
+    console.log(usernameInput);
+
+    const emailInput = document.getElementById('email-input').value;
+    console.log(emailInput);
+
+    const destinasi = document.getElementById('destinasi').value;
+    console.log(destinasi);
+
+    if (usernameInput == '') {
+        alert('Form username mohon diisi');
+    } else if (emailInput == '') {
+        alert('Form email mohon diisi');
+    } else if (destinasi == '') {
+        alert('Form destinasi mohon diisi');
+    } else
+    {
+        alert ('Sukses mengirim form');
+    }
+}
+
+let indexSlide = 1;
+const listImages = document.getElementsByClassName('banner-autoslide');
+
+showSlide();
+
+function nextSlide() {
+    showSlide(indexSlide += 1);
+}
+
+function showSlide(index) {
+    if(index > listImages.length) indexSlide = 1;
+    console.log(indexSlide);
+    hideAllSlide();
+    listImages[indexSlide - 1].style.display = 'block';
+}
+
+// logic untuk menyembunyikan semua slide
+function hideAllSlide() {
+    for (let i = 0; i < listImages.length; i++) {
+        listImages[i].style.display = 'none';
+    }
+}
+
+// Otoomatisasinya
+setInterval(() => nextSlide(), 2000);
